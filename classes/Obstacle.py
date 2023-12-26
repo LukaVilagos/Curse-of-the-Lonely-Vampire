5ccaa05f1e2979930dfe4c0147c6f780ffb3f5e3
Todomii <luka.vilagos@gmail.com>
2023-12-26 12:47:34 +0100

    obstacles work again

--- classes/Obstacle.py
+++ classes/Obstacle.py
@@ -5,8 +5,6 @@ class Obstacle(pg.sprite.Sprite):
         super().__init__(group)
         self.game = game
         self.image = image
-        rect = pg.Rect(0, self.image.get_height() - 10, self.image.get_width(), 10)
-        self.image = pg.transform.chop(self.image, rect)
         self.rect = self.image.get_rect(topleft = pos)
         self.x = pos[0] * self.game.tile_size
         self.y = pos[1] * self.game.tile_size
